756ed516e7e8ccc4856b91238983ca25bb031b52
Shavir <shavirvallabh05@gmail.com>
2025-12-23 22:51:06 +0200

    added move map, needs to be tested

--- cube.py
+++ cube.py
@@ -13,12 +13,78 @@ class Cubie:
         
         self.position = position
         self.faces = faces
-        
+
+"""
+      | y+
+      |
+      |
+     / \           
+z+ /     \ x+
+ /         \
+     
+"""   
 
 class Cube:
     def __init__(self):
         self.cubies = []
         self.build_solved()
+        
+        #for use with Cube.rotate(), easy way to define all possible moves
+        self.MOVE_MAP = {
+            "R":  [('x', 1,  1)],
+            "R'": [('x', 1, -1)],
+            "R2": [('x', 1,  2)],
+            
+            "L":  [('x', -1, -1)],
+            "L'": [('x', -1,  1)],
+            "L2": [('x', -1,  2)],
+            
+            "U":  [('y', 1,  1)],
+            "U'": [('y', 1, -1)],
+            "U2": [('y', 1,  2)],
+            
+            "D":  [('y', -1, -1)],
+            "D'": [('y', -1,  1)],
+            "D2": [('y', -1,  2)],
+            
+            "F":  [('z', 1,  1)],
+            "F'": [('z', 1, -1)],
+            "F2": [('z', 1,  2)],
+            
+            "B":  [('z', -1, -1)],
+            "B'": [('z', -1, 1)],
+            "B2": [('z', -1, 2)],    
+            
+            #slice moves
+            
+            "M":  [('x', 0, -1)],
+            "M'": [('x', 0,  1)],
+            "M2": [('x', 0,  2)],
+            
+            "E":  [('y', 0, -1)],
+            "E'": [('y', 0,  1)],
+            "E2": [('y', 0,  2)],
+            
+            "S":  [('z', 0,  1)],  
+            "S":  [('z', 0, -1)],  
+            "S":  [('z', 0,  2)],  
+            
+            #full rotations are implemented as a composite of other rotations (X == R + M' + L' )
+            
+            "X":  [('x', 1,  1), ('x', 0,  1), ('x', -1, -1)],
+            "X'": [('x', 1, -1), ('x', 0, -1), ('x', -1, -1)],
+            "X'": [('x', 1,  2), ('x', 0,  2), ('x', -1,  2)],
+            
+            "Y": [('y', 1,  1), ('y', 0,  1), ('y', -1,  1)],
+            "Y'": [('y', 1, -1), ('y', 0, -1), ('y', -1, -1)],
+            "Y2": [('y', 1,  2), ('y', 0,  2), ('y', -1,  2)],
+            
+            "Z":  [('z', 1,  1), ('z', 0,  1), ('z', -1,  1)],
+            "Z'": [('z', 1, -1), ('z', 0, -1), ('z', -1, -1)],
+            "Z2": [('z', 1,  2), ('z', 0,  2), ('z', -1,  2)],
+            
+            
+        }
         
     def build_solved(self):
         
@@ -32,42 +98,32 @@ class Cube:
                     #construct faces from coords
                     #white on top, green left, red right
                     faces = {}
-                    if x == 1:  faces['x+'] = 'R'
-                    if x == -1: faces['x-'] = 'O'
+                    if x == 1:  faces['x+'] = 'G'
+                    if x == -1: faces['x-'] = 'B'
                     if y == 1:  faces['y+'] = 'W'
                     if y == -1: faces['y-'] = 'Y'
-                    if z == 1:  faces['z+'] = 'G'
-                    if z == -1: faces['z-'] = 'B'
+                    if z == 1:  faces['z+'] = 'R'
+                    if z == -1: faces['z-'] = 'O'
                     
                     self.cubies.append(Cubie(coords, faces))
                     
+    #parse an algorithm from a sting (R L' U2 etc.)
     def parse_sequence(self, sequence):
-        #parse an algorithm from a sting (r,l',u2, etc.)
-        print("parse")
+        moves = sequence.split()
+        for move in moves:
+            if not self.MOVE_MAP[move]:
+                print(f"Unknown move {move}.")
+                continue 
+            self.rotate(self.MOVE_MAP[move])
+        print(f"sequence {sequence} parsed and applied!")
            
-    def r(self):
-        print('r')
-    
-    def r_prime(self):
-        print('r_prime')
-    
-    def u(self):
-        print('u')
-    
-    def u_prime(self):
-        print("u_prime")
-    
-    def d(self):
-        print('d')
-    
-    def d_prime(self):
-        print("d_prime")
-    
-    def l(self):
-        print('l')
-        
-    def l_prime(self):
-        print('l_prime')
+    def rotate(axis, layer, direction):
+        """ works with the move_map to apply transformations to the cube
+        Args:
+            axis a char {x, y, z}: axis to rotate around
+            layer an int {-1, 0, 1}: layer to rotate
+            direction an int {1, -1, 2}: regular, prime, double move
+        """
         
                     
                     
